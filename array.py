05d99dc502f86ff02c65da0ea6362cfb23c6eca1
conorrific <conorbellchambers@gmail.com>
2018-05-03 10:05:33 -0500

    begging of arrays lecture

---
+++ array.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+#make the array name plural so that it sounds more natural
+numbers = [34,56,12,78,56,687]
+
+for index in range(9,4):
+    print(f"index is {index} and value is {names[index]}")
+    
+
+
+
+
